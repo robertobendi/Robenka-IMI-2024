d1ec154e81783c97a1aefff3fc855fddba99fc84
Roberto Bendinelli <56977459+robertobendi@users.noreply.github.com>
2024-09-28 11:51:55 +0200

    playble prototype

--- Assets/SCRIPTS/PanelManager.cs
+++ Assets/SCRIPTS/PanelManager.cs
@@ -1,41 +1,132 @@
 using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using TMPro;
 
 public class PanelManager : MonoBehaviour
 {
+    [SerializeField] private GameObject basePanel;
     [SerializeField] private GameObject dayPassedPanel;
     [SerializeField] private GameObject dayFailedSpyPanel;
     [SerializeField] private GameObject dayFailedNotEnoughPeoplePanel;
     [SerializeField] private GameObject gameWonPanel;
+    
+    [SerializeField] private GameObject dayIntroPanel;
+    [SerializeField] private TextMeshProUGUI dayNumberText;
+    [SerializeField] private TextMeshProUGUI personsNeededText;
+    
+    [SerializeField] private float fadeDuration = 0.5f;
+
+    private CanvasGroup basePanelCanvasGroup;
+    private CanvasGroup dayPassedCanvasGroup;
+    private CanvasGroup dayFailedSpyCanvasGroup;
+    private CanvasGroup dayFailedNotEnoughPeopleCanvasGroup;
+    private CanvasGroup gameWonCanvasGroup;
+    private CanvasGroup dayIntroPanelCanvasGroup;
+
+    private void Awake()
+    {
+        basePanelCanvasGroup = GetOrAddCanvasGroup(basePanel);
+        dayPassedCanvasGroup = GetOrAddCanvasGroup(dayPassedPanel);
+        dayFailedSpyCanvasGroup = GetOrAddCanvasGroup(dayFailedSpyPanel);
+        dayFailedNotEnoughPeopleCanvasGroup = GetOrAddCanvasGroup(dayFailedNotEnoughPeoplePanel);
+        gameWonCanvasGroup = GetOrAddCanvasGroup(gameWonPanel);
+        dayIntroPanelCanvasGroup = GetOrAddCanvasGroup(dayIntroPanel);
+
+        HideAllPanels();
+    }
+
+    private CanvasGroup GetOrAddCanvasGroup(GameObject obj)
+    {
+        CanvasGroup cg = obj.GetComponent<CanvasGroup>();
+        if (cg == null)
+        {
+            cg = obj.AddComponent<CanvasGroup>();
+        }
+        return cg;
+    }
 
     private void HideAllPanels()
     {
-        dayPassedPanel.SetActive(false);
-        dayFailedSpyPanel.SetActive(false);
-        dayFailedNotEnoughPeoplePanel.SetActive(false);
-        gameWonPanel.SetActive(false);
+        SetPanelActive(basePanelCanvasGroup, false);
+        SetPanelActive(dayPassedCanvasGroup, false);
+        SetPanelActive(dayFailedSpyCanvasGroup, false);
+        SetPanelActive(dayFailedNotEnoughPeopleCanvasGroup, false);
+        SetPanelActive(gameWonCanvasGroup, false);
+        SetPanelActive(dayIntroPanelCanvasGroup, false);
+    }
+
+    private void SetPanelActive(CanvasGroup cg, bool active)
+    {
+        cg.alpha = active ? 1 : 0;
+        cg.gameObject.SetActive(active);
+    }
+
+    public IEnumerator ShowDayIntroPanel(int dayNumber, int personsNeeded)
+    {
+        dayNumberText.text = dayNumber.ToString();
+        personsNeededText.text = personsNeeded.ToString();
+        
+        dayIntroPanel.SetActive(true);
+        dayIntroPanelCanvasGroup.alpha = 0;
+        
+        yield return dayIntroPanelCanvasGroup.DOFade(1, fadeDuration).WaitForCompletion();
+    }
+
+    public IEnumerator FadeOutDayIntroPanel()
+    {
+        yield return dayIntroPanelCanvasGroup.DOFade(0, fadeDuration).WaitForCompletion();
+        dayIntroPanel.SetActive(false);
+    }
+
+    private IEnumerator ShowPanelWithDelay(CanvasGroup panelToShow)
+    {
+        HideAllPanels();
+        
+        SetPanelActive(basePanelCanvasGroup, true);
+        yield return FadeInPanel(basePanelCanvasGroup);
+
+        yield return FadeInPanel(panelToShow);
+    }
+
+    private IEnumerator FadeInPanel(CanvasGroup canvasGroup)
+    {
+        canvasGroup.gameObject.SetActive(true);
+        canvasGroup.alpha = 0;
+        yield return canvasGroup.DOFade(1, fadeDuration).WaitForCompletion();
     }
 
     public void ShowDayPassedPanel()
     {
-        HideAllPanels();
-        dayPassedPanel.SetActive(true);
+        StartCoroutine(ShowPanelWithDelay(dayPassedCanvasGroup));
     }
 
     public void ShowDayFailedSpyPanel()
     {
-        HideAllPanels();
-        dayFailedSpyPanel.SetActive(true);
+        StartCoroutine(ShowPanelWithDelay(dayFailedSpyCanvasGroup));
     }
 
     public void ShowDayFailedNotEnoughPeoplePanel()
     {
-        HideAllPanels();
-        dayFailedNotEnoughPeoplePanel.SetActive(true);
+        StartCoroutine(ShowPanelWithDelay(dayFailedNotEnoughPeopleCanvasGroup));
     }
 
     public void ShowGameWonPanel()
     {
+        StartCoroutine(ShowPanelWithDelay(gameWonCanvasGroup));
+    }
+
+    public IEnumerator FadeOutAllPanels()
+    {
+        Sequence fadeOutSequence = DOTween.Sequence();
+        fadeOutSequence.Join(basePanelCanvasGroup.DOFade(0, fadeDuration));
+        fadeOutSequence.Join(dayPassedCanvasGroup.DOFade(0, fadeDuration));
+        fadeOutSequence.Join(dayFailedSpyCanvasGroup.DOFade(0, fadeDuration));
+        fadeOutSequence.Join(dayFailedNotEnoughPeopleCanvasGroup.DOFade(0, fadeDuration));
+        fadeOutSequence.Join(gameWonCanvasGroup.DOFade(0, fadeDuration));
+
+        yield return fadeOutSequence.WaitForCompletion();
+
         HideAllPanels();
-        gameWonPanel.SetActive(true);
     }
 }
